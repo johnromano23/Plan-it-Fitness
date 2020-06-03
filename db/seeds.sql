781cd9f4603989372e43292f2d3a6c18e483f0b3
Doris Castro <dorisgeo12@gmail.com>
2020-06-03 12:54:00 -0400

    added to schema and seed sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO beginner (name,sets,reps,break) VALUES ('Push-up', 3, 10, 1);
+
+INSERT INTO intermediate (name_id, sets, reps, break) VALUES (1, 5, 15, 2);
+
+INSERT INTO advance (name_id, sets, reps, break) VALUES (1, 7, 20, 3);
