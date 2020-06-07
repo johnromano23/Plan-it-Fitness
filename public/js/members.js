54a75a1e59e2cf55a68730806907193278b698b9
John_Romano <romanoj723@gmail.com>
2020-06-07 16:12:54 -0400

    new members page

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
+function BMI() {
+  var w = document.getElementById("w").value;
+
+  var h = document.getElementById("h").value;
+
+  var bmi = (w / h / h) * 703;
+
+  document.getElementById("result").innerHTML = "Your BMI is " + bmi;
+}
+
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
   });
 });
